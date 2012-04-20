1573b82ceed5d6d21034f7f0b2ce172236604730
Alejandro Romero <alejo8591@gmail.com>
2012-04-20 16:48:47 -0500

    update urls in pressman

--- pressman/urls.py
+++ pressman/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
+
+from pressman import views
 
 urlpatterns = patterns('',
-                       (r'^detail/(?<pid>\d+)/$', 'pressman.views.detail'),
+       url(r'^detail/(?P<pid>\d+)/$', 'pressman.views.detail'),
 )
-
