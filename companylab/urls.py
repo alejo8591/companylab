ead5ef572aa7abbd02ddf35c21463c350c62bfc6
Alejandro Romero <alejo8591@gmail.com>
2012-04-20 16:47:31 -0500

    update generals urls

--- companylab/urls.py
+++ companylab/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import *
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^pressman/', 'pressman.urls'),
+    url(r'^pressman/', include('pressman.urls')),
 )
