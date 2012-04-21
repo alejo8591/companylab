78e38d2e35e6497d26c7e4fdef12d31649505fcf
Alejandro Romero <alejo8591@gmail.com>
2012-04-21 00:48:19 -0500

    update urls for pressman app

--- pressman/urls.py
+++ pressman/urls.py
@@ -5,5 +5,8 @@ from pressman import views
 
 urlpatterns = patterns('',
        url(r'^detail/(?P<pid>\d+)/$', 'pressman.views.detail'),
-       url(r'^list/', 'pressman.views.pressman_list'),
+       url(r'^latest/$', 'pressman.views.latest'),
+       url(r'^list/$', 'pressman.views.pressman_list'),
+       url(r'^$', 'django.views.generic.simple.redirect_to',
+           {'url':'/pressman/list/'}),
 )
