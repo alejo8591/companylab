9d734219933c44ba70c3ac998a5b31da67563318
Alejandro Romero <alejo8591@gmail.com>
2012-04-20 23:04:38 -0500

    update view with list

--- pressman/views.py
+++ pressman/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from pressman.models import PressRelease
+from django.shortcuts import get_object_or_404, get_list_or_404
+from django.template import loader, Context
 
 def detail(request, pid):
-    try:
-        p = PressRelease.objects.get(id=pid)
-        return HttpResponse(p.pr_title)
-    except PressRelease.DoesNotExist:
-        return HttpResponseNotFound('No existe lo que esta buscando')
+    p = get_object_or_404(PressRelease, id=pid)
+    t = loader.get_template('pressman/detail.html')
+    c = Context({'pressman':p})
+    return HttpResponse(t.render(c))
+    
+def pressman_list(request):
+    pl = get_list_or_404(PressRelease)
+    t = loader.get_template('pressman/list.html')
+    c = Context({'pressman_list':pl})
+    return HttpResponse(t.render(c))
+    
