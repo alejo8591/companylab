3fe31011a7c549fba90d3cec817f6f6ca7ed5701
Alejandro Romero <alejo8591@gmail.com>
2012-04-21 00:46:34 -0500

    update with add robot.txt

--- companylab/urls.py
+++ companylab/urls.py
@@ -15,4 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^pressman/', include('pressman.urls')),
+    url(r'^robots.txt', 'django.views.generic.simple.direct_to_template',
+        {'template': 'robots.txt'})
 )
