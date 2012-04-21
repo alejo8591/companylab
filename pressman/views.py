5465389b50a51bee6bcd694d1eb0db29451d65d2
Alejandro Romero <alejo8591@gmail.com>
2012-04-20 21:39:38 -0500

    update view with HttpResponse

--- pressman/views.py
+++ pressman/views.py
@@ -1,4 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from pressman.models import PressRelease
 
 def detail(request, pid):
-    return HttpResponse('mycompanylab is a project cool')
+    try:
+        p = PressRelease.objects.get(id=pid)
+        return HttpResponse(p.pr_title)
+    except PressRelease.DoesNotExist:
+        return HttpResponseNotFound('No existe lo que esta buscando')
