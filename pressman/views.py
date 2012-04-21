027083d05db2f397ee81bbb89bd3e7bfaf4436ea
Alejandro Romero <alejo8591@gmail.com>
2012-04-21 01:44:04 -0500

    update with render_to_response in views for latest

--- pressman/views.py
+++ pressman/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from pressman.models import PressRelease
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404, render_to_response
 from django.template import loader, Context
 from pressman.models import PressMan, PressRelease
 
@@ -19,11 +19,4 @@ def pressman_list(request):
 def latest(request):
     """ Retornando info del ultimo post """
     p = PressRelease.objects.latest()
-    t = loader.get_template('pressman/latest.html')
-    c = Context({
-        'title':p.pr_title,
-        'author':p.pr_author,
-        'date':p.pr_date_created,
-        'link':p.get_absolute_url()
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('pressman/latest.html',{ 'pressman':p, })
