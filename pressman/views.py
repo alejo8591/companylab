c3949c3cb4eff40c8bfa1951ee7ad5887a0e28e9
Alejandro Romero <alejo8591@gmail.com>
2012-04-21 00:48:51 -0500

    update views for pressman app

--- pressman/views.py
+++ pressman/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 from pressman.models import PressRelease
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.template import loader, Context
+from pressman.models import PressMan, PressRelease
 
 def detail(request, pid):
     p = get_object_or_404(PressRelease, id=pid)
@@ -15,3 +16,14 @@ def pressman_list(request):
     c = Context({'pressman_list':pl})
     return HttpResponse(t.render(c))
     
+def latest(request):
+    """ Retornando info del ultimo post """
+    p = PressRelease.objects.latest()
+    t = loader.get_template('pressman/latest.html')
+    c = Context({
+        'title':p.pr_title,
+        'author':p.pr_author,
+        'date':p.pr_date_created,
+        'link':p.get_absolute_url()
+    })
+    return HttpResponse(t.render(c))
